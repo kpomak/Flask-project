b25993752e55d7eb0e9fd48a4f58014cdea869a7
kpomak <romka_ne@mail.ru>
2022-12-12 02:02:14 +0300

    merged feature-1

--- newspapper/app.py
+++ newspapper/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Flask web-application"
+@app.route("/<name>")
+def index(name: str):
+    return f"Flask web-application '{name}'"
+
