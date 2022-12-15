ef953bc67b83ab823fff44d34c3b80eda77806e1
kpomak <romka_ne@mail.ru>
2022-12-15 18:55:22 +0300

    create db and user commands

---
+++ newspapper/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from newspapper.models.database import db
+
+
+class CustomUser(db.Model):
+    id = Column(Integer, primary_key=True)
+    username = Column(String(255), unique=True, nullable=False)
+    is_staff = Column(Boolean, nullable=False, default=False)
+
+    def __repr__(self) -> str:
+        return f"<CustomUser {self.id} {self.username}"
