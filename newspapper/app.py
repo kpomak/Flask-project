c2da814e8777e8bb566a1df654c07475bc285738
kpomak <romka_ne@mail.ru>
2022-12-15 17:51:58 +0300

    aricle details

--- newspapper/app.py
+++ newspapper/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from newspapper.views.users import users_app
+from newspapper.views.articles import articles_app
 
 app = Flask(__name__)
 
 app.register_blueprint(users_app, url_prefix='/users')
+app.register_blueprint(articles_app, url_prefix='/articles')
 
 @app.route("/")
 def index():
---
+++ newspapper/views/articles.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template
+from werkzeug.exceptions import NotFound
+
+articles_app = Blueprint("articles_app", __name__)
+
+ARTICLES = [
+    "Flask",
+    "Django",
+    "FastAPI",
+    ]
+
+@articles_app.route("/", endpoint="list")
+def articles_list():
+    return render_template("articles/list.html", articles=ARTICLES)
+
+
+@articles_app.route("/<string:title>/", endpoint="details")
+def aricle_details(title: str):
+    if title in ARTICLES:
+        return render_template(
+        'articles/details.html',
+        article_title=title,
+        )
+    raise NotFound(f"Article {title} doesn't exists! 😢")
