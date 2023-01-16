ab962b00ed4ab55fd4118308229c5de366797395
kpomak <romka_ne@mail.ru>
2023-01-16 12:02:27 +0300

    registration form

---
+++ newspapper/forms/user.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField, validators
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", [validators.DataRequired()])
+    first_name = StringField("First Name")
+    last_name = StringField("Last Name")
+    email = StringField(
+        "email",
+        [validators.DataRequired(), validators.Email()],
+        filters=[lambda data: data and data.lower()],
+    )
+    password = PasswordField(
+        "Password", [validators.DataRequired(), validators.EqualTo("confirm_password")]
+    )
+    confirm_password = PasswordField("Confirm password", [validators.DataRequired()])
+    submit = SubmitField("Register")
--- newspapper/views/auth.py
+++ newspapper/views/auth.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask import (Blueprint, current_app, flash, redirect, render_template,
+                   request, url_for)
 from flask_login import (LoginManager, current_user, login_required,
                          login_user, logout_user)
+from sqlalchemy.exc import IntegrityError
 from werkzeug.security import check_password_hash
 
+from newspapper.forms.user import RegistrationForm
 from newspapper.models import CustomUser
+from newspapper.models.database import db
 
 auth_app = Blueprint("auth_app", __name__)
 
@@ -14,6 +18,44 @@ __all__ = [
     "login_manager",
     "auth_app",
 ]
+
+
+@auth_app.route("/register/", methods=["GET", "POST"], endpoint="register")
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
+    error = None
+    form = RegistrationForm(request.form)
+
+    if request.method == "POST" and form.validate_on_submit():
+        if CustomUser.query.filter_by(username=form.username.data).count():
+            form.username.errors.append("username already exists!")
+            return render_template("auth/register.html", form=form)
+
+        if CustomUser.query.filter_by(email=form.email.data).count():
+            form.email.errors.append("email already exists!")
+            return render_template("auth/register.html", form=form)
+
+        user = CustomUser(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            username=form.username.data,
+            password=form.password.data,
+            email=form.email.data,
+            is_staff=False,
+        )
+        db.session.add(user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            current_app.logger.exception("Could not create user!")
+            error = "Could not create user!"
+        else:
+            current_app.logger.info(f"Created user {user}")
+            login_user(user)
+            return redirect(url_for("index"))
+    return render_template("auth/register.html", form=form, error=error)
 
 
 @login_manager.user_loader
