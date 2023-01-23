9c8c71f8befe4042ee29e3241eb8936818178815
kpomak <romka_ne@mail.ru>
2023-01-24 01:01:01 +0300

    Tag model added

--- newspapper/models/article.py
+++ newspapper/models/article.py
@@ -4,6 +4,7 @@ from sqlalchemy import (Column, DateTime, ForeignKey, Integer, String, Text,
                         func)
 from sqlalchemy.orm import relationship
 
+from newspapper.models.article_tag import article_tag_association_table
 from newspapper.models.database import db
 
 
@@ -19,3 +20,8 @@ class Article(db.Model):
     dt_updated = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     author = relationship("Author", back_populates="articles")
+    tags = relationship(
+        "Tag",
+        secondary=article_tag_association_table,
+        back_populates="articles",
+    )
