4a308ef4360b38394e2a27083ff826c494a958d0
kpomak <romka_ne@mail.ru>
2023-01-29 12:43:19 +0300

    swagger

--- newspapper/api/__init__.py
+++ newspapper/api/__init__.py
@@ -1,9 +1,22 @@
+from combojsonapi.spec import ApiSpecPlugin
 from flask_combo_jsonapi import Api
-from newspapper.api.tag import TagList, TagDetail
+
+from newspapper.api.tag import TagDetail, TagList
+
+
+def create_api_spec_plugin(app):
+    api_spec_plugin = ApiSpecPlugin(
+        app=app,
+        tags={
+        "Tag": "Tag API",
+        }
+    )
+    return api_spec_plugin
 
 
 def init_api(app):
-    api = Api(app)
+    api_spec_plugin = create_api_spec_plugin(app)
+    api = Api(app=app, plugins=[api_spec_plugin])
     api.route(TagList, "tag_list", "/api/tags/")
     api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/")
     return api
