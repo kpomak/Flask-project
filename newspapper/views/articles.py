0f0d0f10b8d2903662d96b7528a866eb22da2e7d
kpomak <romka_ne@mail.ru>
2023-01-31 19:51:10 +0300

    the dumbest view in the whole world

--- newspapper/views/articles.py
+++ newspapper/views/articles.py
@@ -1,8 +1,7 @@
 import requests
 import os
 
-from flask import (Blueprint, current_app, redirect, render_template, request,
-                   url_for)
+from flask import Blueprint, current_app, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import joinedload
@@ -24,21 +23,55 @@ articles_app = Blueprint("articles_app", __name__)
 
 @articles_app.route("/", endpoint="list")
 def articles_list():
+    """
+    the dumbest view in the whole world =P
+    """
     BASE_URL = os.getenv("BASE_URL")
-    api_url = BASE_URL + '/api/articles/?include=author%2Ctags&fields%5Barticle%5D=id,title,body,author,tags&fields%5Bauthor%5D=id,user&fields%5Btag%5D=name,id'
-    response = requests.get(api_url).json()
-    articles_list = response["data"]
-    articless = [
+    articles_url = (
+        BASE_URL
+        + "/api/articles/?include=author%2Ctags&fields%5Barticle%5D=id,title,body,author,tags&fields%5Bauthor%5D=id,user&fields%5Btag%5D=name,id"
+    )
+    authors_url = (
+        BASE_URL
+        + "/api/authors/?include=user%2Carticles&fields%5Bauthor%5D=id,user&fields%5Buser%5D=first_name,id,last_name"
+    )
+    articles_response = requests.get(articles_url).json()
+    authors_response = requests.get(authors_url).json()
+
+    authors_list = {
+        author["id"]: author["relationships"]["user"]["data"]["id"]
+        for author in authors_response["data"]
+    }
+
+    users = {
+        user["id"]: {
+            "user": {
+                "first_name": user["attributes"].get("first_name"),
+                "last_name": user["attributes"].get("last_name"),
+            }
+        }
+        for user in authors_response["included"]
+    }
+    tags = {
+        item["id"]: {"name": item["attributes"].get("name")}
+        for item in articles_response["included"]
+        if item["type"] == "tag"
+    }
+    articles = [
         {
             "id": item["id"],
             "title": item["attributes"]["title"],
             "body": item["attributes"]["body"],
-            "author": item["relationships"]["author"],
-            "tags": item["relationships"]["tags"],
+            "author": users[
+                authors_list[item["relationships"]["author"]["data"]["id"]]
+            ],
+            "tags": [
+                tags[tag.get("id")]
+                for tag in item.get("relationships").get("tags").get("data")
+            ],
         }
-        for item in articles_list
+        for item in articles_response["data"]
     ]
-    articles = Article.query.options(joinedload(Article.tags)).all()
     return render_template("articles/list.html", articles=articles)
 
 
