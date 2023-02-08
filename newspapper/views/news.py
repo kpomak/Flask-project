1bac0c03520bfa449a6df1ccd469664f6d4d8ec0
kpomak <romka_ne@mail.ru>
2023-02-08 15:17:02 +0300

    lenta.ru news

--- newspapper/views/news.py
+++ newspapper/views/news.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template
 
+from newspapper.utils.news import lenta_ru_parser
+
 
 news_app = Blueprint("news_app", __name__)
 
 
 @news_app.route("/", endpoint="list")
 def news_list():
-    news = [{"title": "заголовок", "body": "статья"}]
+    news = lenta_ru_parser()
     return render_template("news/list.html", news=news)
